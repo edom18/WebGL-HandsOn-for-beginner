80be2b9974ad55e5847f2809248452c1c6be01dc
edom18 <edo.m18@gmail.com>
2016-03-22 20:33:34 +0900

    モデルの表示と影の表示

--- js/main.js
+++ js/main.js
@@ -22,64 +22,85 @@
     // カメラを生成
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
     camera.position.x = 0;
-    camera.position.y = 0.1;
-    camera.position.z = 0;
-    // camera.lookAt(new THREE.Vector3(0, 0, 0));
+    camera.position.y = 1.0;
+    camera.position.z = 5.0;
 
+    var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
     //////////////////////////////////////////////////
     // 各種オブジェクトのセットアップ
 
-    // Skysphereの生成
-    var skysphereLoader = new THREE.TextureLoader();
-    function onSkysphereTextureLoaded(texture) {
-        texture.wrapS = THREE.RepeatWrapping;
-        texture.wrapT = THREE.RepeatWrapping;
-        texture.repeat.set(4, 4);
+    // ベッド
+    var bedLoader = new THREE.JSONLoader();
+    bedLoader.load('models/bed.json', function (geometry, materials) {
+        var material = new THREE.MeshFaceMaterial(materials);
+        var bed = new THREE.Mesh(geometry, material);
+        var s = 0.5;
+        bed.scale.set(s, s, s);
+        bed.castShadow = true;
+        bed.receiveShadow = true;
+        scene.add(bed);
+    });
 
-        var geometry = new THREE.SphereGeometry(5000, 128, 128);
-        var material = new THREE.MeshBasicMaterial({
-            map: texture,
-            color: 0xffffff,
-            side: THREE.BackSide
-        });
-
-        var skysphere = new THREE.Mesh(geometry, material);
-        skysphere.position.z = 0;
-        scene.add(skysphere);
-    }
-    skysphereLoader.load('img/bg_skyplane.png', onSkysphereTextureLoaded);
+    var tableLoader = new THREE.JSONLoader();
+    tableLoader.load('models/table.json', function (geometry, materials) {
+        var material = new THREE.MeshFaceMaterial(materials);
+        var table = new THREE.Mesh(geometry, material);
+        var s = 0.25;
+        table.scale.set(s, s, s);
+        table.position.x = 1.2;
+        table.castShadow = true;
+        scene.add(table);
+    });
 
     // ライトの生成
     var light = new THREE.DirectionalLight(0xffffff);
-    light.castShadow = true;
     light.position.set(1, 1, 1);
-    light.shadow.mapSize.width = 2048;
-    light.shadow.mapSize.height = 2048;
+    light.castShadow = true;
+    light.shadow.mapSize.width  = 4096;
+    light.shadow.mapSize.height = 4096;
     scene.add(light);
 
-    var s = 0.1;
-    var geometry = new THREE.BoxGeometry(s, s, s);
-    var material = new THREE.MeshLambertMaterial({
-        color: 0x99ccff
+    var ambient = new THREE.AmbientLight(0x333333);
+    scene.add(ambient);
+
+
+    var floorTextureLoader = new THREE.TextureLoader();
+    floorTextureLoader.load('models/Sapele Mahogany.jpg', function (texture) {
+        texture.repeat.set(4, 4);
+
+        var planeGeo = new THREE.PlaneGeometry(5, 5);
+        var planeMat = new THREE.MeshLambertMaterial({
+            color: 0xffffff,
+            map: texture
+        });
+        var plane = new THREE.Mesh(planeGeo, planeMat);
+        plane.receiveShadow = true;
+        plane.rotation.x = -Math.PI / 2;
+        scene.add(plane);
     });
-    var cube = new THREE.Mesh(geometry, material);
-    cube.position.z = -1;
-    cube.castShadow = true;
-    cube.receiveShadow = true;
-    scene.add(cube);
 
-
-    var planeGeo = new THREE.PlaneGeometry(1, 1);
+    var planeGeo = new THREE.PlaneGeometry(5, 2.5);
     var planeMat = new THREE.MeshLambertMaterial({
-        color: 0xdddddd
+        color: 0xffffff
     });
     var plane = new THREE.Mesh(planeGeo, planeMat);
-    plane.receiveShadow = true;
-    plane.position.z = -1;
-    plane.position.y = -0.1;
-    plane.rotation.x = -Math.PI / 2;
+    plane.position.z = -2.5;
+    plane.position.y = 1.25;
     scene.add(plane);
+
+    var leftWall = plane.clone();
+    leftWall.position.z = 0;
+    leftWall.position.x = -2.5;
+    leftWall.rotation.y = Math.PI / 2;
+    scene.add(leftWall);
+
+    var rightWall = plane.clone();
+    rightWall.position.z = 0;
+    rightWall.position.x = 2.5;
+    rightWall.rotation.y = -Math.PI / 2;
+    scene.add(rightWall);
+
 
 
     //////////////////////////////////////////////////
@@ -87,9 +108,6 @@
     // アニメーションループ
     function animate(timestamp) {
         renderer.render(scene, camera);
-
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.02;
 
         // アニメーションループ
         requestAnimationFrame(animate);
